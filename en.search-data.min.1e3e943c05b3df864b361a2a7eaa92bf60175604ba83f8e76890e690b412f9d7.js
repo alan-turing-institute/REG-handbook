2593ecb979e1e455db9a7813f94a50a59f3e8d03
JimMadge <JimMadge@users.noreply.github.com>
2021-11-16 14:02:20 +0000

    deploy: de8d4c85cb8fdbe20905e1ae0d1ff8fc450dfd77

---
+++ en.search-data.min.1e3e943c05b3df864b361a2a7eaa92bf60175604ba83f8e76890e690b412f9d7.js
@@ -0,0 +1,1 @@
+'use strict';(function(){const a={cache:!0};a.doc={id:'id',field:['title','content'],store:['title','href','section']};const b=FlexSearch.create('balance',a);window.bookSearchIndex=b})()
