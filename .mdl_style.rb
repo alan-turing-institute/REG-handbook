9821e8d73b95d7e0c591804ff391bc13bf7e3358
Jim Madge <jim@jmadge.com>
2022-05-27 14:47:13 +0100

    Tidy mdl style

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -9,7 +9,7 @@ rule 'MD003', :style => :atx
 # Only allow dashes in unordered lists
 rule 'MD004', :style => :dash
 
-# Do not enforce line length to make it easier to cut and paste content from other editors.
+# Do not enforce line length
 exclude_rule 'MD013'
 
 # Ignore blockquotes separated only be a blank line. This is a limitation of
